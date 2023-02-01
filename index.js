004bedd4047603754bf4127e4cfb4667a1d28097
Fatlum Hasaj <fatlum.hasaj@outlook.com>
2023-02-01 15:50:32 +0000

    Coming Soon page

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const router = express.Router();
 const port = process.env.PORT || 3000;
 
-
-app.get('/', (req, res) => {
-    res.send("Hello World!");
+///Basic Routing until it is fixed
+router.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
+
+
+app.get('/', router);
 
 app.listen(port, () => {
     console.log(`Listing on port ${port}`);
