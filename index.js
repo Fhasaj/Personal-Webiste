92ad8cdf350a8af683c21c373df408e6296c20e3
Fatlum Hasaj <fatlum.hasaj@outlook.com>
2023-02-02 13:08:35 +0000

    Update so background image shows

--- index.js
+++ index.js
@@ -4,14 +4,17 @@ const path = require('path');
 const router = express.Router();
 const port = process.env.PORT || 3000;
 
-///Basic Routing until it is fixed
-router.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+
+//view engine setup
+app.set("views", path.join(__dirname, "/views"));
+app.set("view engine", "hbs");
+app.use(express.static("views"))
+
+//Routing Setup
+app.get('/', (req, res) => {
+    res.render('index');
 });
-
-
-app.get('/', router);
 
 app.listen(port, () => {
     console.log(`Listing on port ${port}`);
-})
+});
