25ec6d32fbe6a18f8ccf20103bb633fe1d58897b
Fatlum Hasaj <fatlum.hasaj@outlook.com>
2023-03-15 11:50:46 +0000

    Init blog page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Projects from "./components/Projects";
+import Blog from "./components/Blog";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
     <Hero />
     <About />
     <Projects />
+    <Blog />
     </>
   );
 }
